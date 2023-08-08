86bec57a221ce205a392fbd85b7fd882c53ac895
joekariuki3 <joekariuki3@gmail.com>
2023-08-08 16:00:39 +0300

    Added Test cases for BaseModel
    worked on test cases of the BaseModel
    its atributes
    also the methods tests are all passing

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,53 @@
+#!/usr/bin/python3
+
+"""
+get the uuid for ad=signing uniq id to each instance created
+get datetime to record created and updated time of each object
+get json for use in serialization and deserialization
+"""
+
+
+import uuid
+from datetime import datetime
+import json
+
+
 """
 BaseModal class
 """
-import uuid
-from datetime import datetime,timedelta
-import json
+
 
 class BaseModel():
     """
     initiate the class with these atributes
-    @id - uniq id 
+    @id - uniq id
     @created_at - time for instance creation
     """
     def __init__(self):
+        """
+        initiate the base class
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """ print string rep if object """
-        return(f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
-    
+        return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
+
     def save(self):
         """
         updates the public instance atribute updated_at
         with current datetime
         """
         self.updated_at = datetime.now()
-    
-    def to_dict(self):
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
 
-        return json.dumps(self.__dict__)
+    def to_dict(self):
+        """
+        Return the dictionary of and object
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
