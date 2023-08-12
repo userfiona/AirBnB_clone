0f857353f67bf99042e81818bc24edce43e49c1c
joekariuki3 <joekariuki3@gmail.com>
2023-08-12 23:21:50 +0300

    __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,12 +3,10 @@
 """
 import Filestorage module
 """
-
 from models.engine.file_storage import FileStorage
 
-
-"""create the variable storage, an instance of FileStorage"""
-
-
+"""
+create the variable storage, an instance of FileStorage
+"""
 storage = FileStorage()
 storage.reload()
